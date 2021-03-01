8da8463479044e160a27e22f69390f2ea5a0895d
briannagautama <brianna@connect.hku.hk>
2021-03-01 18:00:54 +0800

    Added Description and revised the code
    Revised the code

--- Fish_Code/Communication_Test_Code/Communication_Test_Code.ino
+++ Fish_Code/Communication_Test_Code/Communication_Test_Code.ino
@@ -1,5 +1,15 @@
+/* 
+    *  Fish Encoder
+    *  
+    *  Receives instructions from transmitter for turning and power values for the fish tail
+    *  
+    *  Created 1 Mar 2021
+    *  By Allison, Brianna, Heidi
+    *  
+*/
+
 //Arduino Pin Declarations
-unsigned int pwm_Pin = 10;//10;
+unsigned int pwm_Pin = 9;//10;
 unsigned int motor_Pwm = 0;
 
 bool red_Flag = LOW;
@@ -7,31 +17,28 @@ bool on = LOW;
 unsigned long int timer1 = 0;
 unsigned long int timer2 = 0;
 
-//Radio Signal 
-char myStr = '0';
-
 long int counter =0;
 long int runtime = 0;
 long int timer = 0;
 float timeStep = 0;
 long int encoderCounter = 0;
 double angularVelocity[3] = {0,0,0};
-int turnVal=0; //Stores commmand value for control left and right. values 1-4 are left, 5 is straight, 6-9 are right
+int turnVal=5; //Stores commmand value for control left and right. values 1-4 are left, 5 is straight, 6-9 are right
+int oldTurnVal=5;
 int encoderVal=0; //stores value of encoder at any given time. Updates from interrupt.
-int power=0; //Stores power value from controller
+int power=5; //Stores power value from controller
+int oldPower=5;
 long int tailTimer1=millis();
 long int tailDelay1=2000;
-//long int tailTimer2=millis();
-//long int tailDelay2=2000;
 int encoderPin0   =  29;
-int offset=0;
-int turningDiff=0.5;
+double diff = 1.0;      //to store the differential value corresponding to the signal
+double stepDiff = 0.1;  //sets the differential value
+
+int highcutoff, lowcutoff, offset,num;
 
 void setup() {
  // Define the Serial ports, with Serial1 as the Receiver Port for Data from the transmitter. 
  // The RC is defined as 8 bits, Odd parity, 1 check digit. Default Arduino setting is SERIAL_8N1, where N means no parity.
- //Serial.begin(9600,SERIAL_8O1);
- //Serial.begin(9600);
  
  // Set the PWM frequency of Timer 2 to be 3kHz
  TCCR2B = TCCR2B & 0b11111000 | 0x01;
@@ -53,107 +60,122 @@ void setup() {
   
   while(!Serial.read());
   timer = micros();
-
-  //attachInterrupt(digitalPinToInterrupt(20), encoder, HIGH);
   
 
   Serial.println("Encoder Pins Initialized!");
   Serial.println("Setup is complete! Click to begin the program....");
 
-  //power = 8;//myStr - '0' ; // This line
-  //turnVal=4;
-  encoder();
+  encoder();      //reads the offset value
   delay(1000);
-  //offset=encoderVal;
+  offset=encoderVal;
   Serial.print("Offset= ");
   Serial.println(offset);
 }
 
 void loop() {
-
-  //Checks if there is any information in the Serial Buffer
-  if(Serial1.available() ){
-    myStr = Serial1.read(); //receives a signal from 0-9
-    //Add code to seperate myStr to appropriate sections and store in variaibles below
-    Serial.println(myStr); 
-    // Converts the character into an integer
-
-    if (myStr>='A' and myStr<='I'){
-      turnVal=myStr-'A'+1; // and this line
+    
+  char incomingByte;
+  int siglen = 0;  //to store the length of the incoming signal
+  char cmd[11];    //to store the signal
+  
+  if (Serial1.available() > 0) {  //if more than 2 bytes available to read in the buffer
+    Serial.print("I'm receiving: ");
+    incomingByte = Serial1.read();
+    while (incomingByte != '?') {  //read char by char until we know the signal is completely read indicated by the identifier '?'
+      cmd[siglen] = incomingByte;
+      Serial.print(incomingByte);
+      siglen++;
+      incomingByte = Serial1.read();
+      Serial1.flush();
     }
-    if (myStr>='0' and myStr<='9')
-      power = myStr - '0' ; // This line
-     
-    timer1 = millis();
-
-    //turnDiff
-    if (turnVal!=5){
-      turningDiff = map((turnVal%5), 1,4,0.5,0.1);
+  }
+  Serial.println();
+  if (checkSum(cmd)==true){
+    //continues only if signal is valid
+    
+    //array[0] is 'T' or 'U' - convert to string, array[1:] are the numbers - convert to integer
+    String w = String(cmd);
+    String letter = String(w[0]);
+    num = String(w.substring(1)).toInt();
+    Serial.print("The number for movement is: ");
+    Serial.println(num);
+    if (letter=="T"){
+      turnVal=num; // turning signal
+    }
+    if (letter=="P"){
+      power = num ; // power signal
+    
+      timer1 = millis();
+      if ((millis()-tailTimer1)%4000>tailDelay1){
+        motor_Pwm = map(power, 0,9,0,255);
     }
     
-    if ((millis()-tailTimer1)%4000>tailDelay1){
-      motor_Pwm = map(power, 0,9,0,255);
+  }
+  encoder();    //reads the current position of the shaft
+  if (oldTurnVal != turnVal && oldPower != power){
+    // sets the range of values for different motor power to generate tail motion corresponding to input signal
+    if ((offset + 512) >1023){
+       lowcutoff = offset+512-1023;
+       highcutoff = offset;
     }
-  }
-  
-  encoder();
-  
-  //Kill switch
-  if (millis() - timer1 > 3000) {
-    motor_Pwm = 0; 
-  }
-  else{
-  //{
-    // Turning control Left
-    if ((turnVal>=1) and (turnVal<=4)){
-      Serial.println("Turning Left");
-      if ((abs(encoderVal-offset)>=206) and (abs(encoderVal-offset)<=306)){
-        if ((millis()-tailTimer1)%4000<tailDelay1){
-            motor_Pwm = map(power, 0,9,0,255)*turningDiff;
+    else{
+       highcutoff = offset + 512;
+       lowcutoff = offset;
+    }
+    Serial.print("highcutoff:");
+    Serial.print(highcutoff);
+    Serial.print("lowcutoff:");
+    Serial.println(lowcutoff);
+    //Kill switch
+    if (millis() - timer1 > 3000) {
+      motor_Pwm = 0; 
+    }
+    else{
+      // Turning control Left
+      if ((turnVal>=1) and (turnVal<=4)){
+        Serial.println("Turning Left");
+        diff = 1-(stepDiff*(5-turnVal));
+        Serial.println(diff);
+        if ((encoderVal < highcutoff) && (encoderVal > lowcutoff)){
+          motor_Pwm = map(power, 0,9,0,255);
         }
-        else{
+        else {
+          motor_Pwm = map(power, 0,9,0,255) * diff;
+        }
+      }
+      
+      // Going Straight
+      else if (turnVal==5){
+        Serial.println("Going straight");
+        motor_Pwm = map(power, 0,9,0,255); //maps the value received (0-9) to (0-255)
+      }
+      
+      // Turning control Right
+      else if ((turnVal>=6) and (turnVal<=9)){
+        Serial.println("Turning Right");
+        diff = 1.0-(stepDiff*(turnVal-5));
+         
+        if ((encoderVal < highcutoff) && (encoderVal > lowcutoff)){
+          motor_Pwm = map(power, 0,9,0,255) * diff;
+        }
+        else {
           motor_Pwm = map(power, 0,9,0,255);
         }
       }
+      analogWrite (pwm_Pin, motor_Pwm);
+      Serial.print("motor pwm: ");
+      Serial.println(motor_Pwm);
+      oldTurnVal = turnVal;
+      oldPower = power;
+      }
     }
-    
-    // Going Straight
-    else if (turnVal==5){
-      Serial.println("Going straight");
-      motor_Pwm = map(power, 0,9,0,255); //maps the value received (0-9) to (0-255)
-    }
-    
-    // Turning control Left
-    else if ((turnVal>=6) and (turnVal<=9)){
-      Serial.println("Turning Right");
-      if ((abs(encoderVal-offset)>=718) and (abs(encoderVal-offset)<=818)){
-        if ((millis()-tailTimer1)%4000<tailDelay1){
-            motor_Pwm = map(power, 0,9,0,255)*turningDiff;
-        }
-        else{
-          motor_Pwm = map(power, 0,9,0,255);
-        }
-      } 
-    }
-  
   }
-  analogWrite (pwm_Pin, motor_Pwm);
-  Serial.print("motor pwm:                    ");
-  Serial.println(motor_Pwm);
-  //delay(500);
 }
 
 
-
-
-
-
-
-
 void encoder() {
+  // returns the position of the shaft (0-1023)
   // put your main code here, to run repeatedly:
-  //int x = PINA; 
-  //int y = PINB;
   int a[10];
   int b[10];
   for(int n = 0 ; n<10 ; n++ ){
@@ -165,33 +187,25 @@ void encoder() {
   }
   float encoderValueRaw = 512.0*b[9]+256.0*b[8]+128.0*b[7]+64.0*b[6]+32.0*b[5]+16.0*b[4]+8.0*b[3]+4.0*b[2]+2.0*b[1]+b[0];
   Serial.print("encodervalue : ");
-  Serial.println(abs(encoderValueRaw-offset));
-  
-  //float angularV;
-  //angularVelocity[0]= angularVelocity[1];
-  //angularVelocity[1] = angularVelocity[2];
-  //angularVelocity[2] = encoderValueRaw;
-    
-  //if(runtime != 0){
-  //  angularV = (angularVelocity[2] - angularVelocity[0])*2*PI/(2*runtime*1024/1000/1000);  //gives the angular velocity in rad/s.
-  //}
-  //Serial.print(angularV);
-  //Serial.print("\t");
-  //Serial.println(encoderValueRaw-600);
-  
-  //if((counter%10) == 0 ) analogWrite(5, encoderValueRaw*255/1024);
-  //if(counter == 1000){
-    //runtime = (micros() - timer)/1000;
-    //Serial.print("the average time for the loop is ");
-    //Serial.print(runtime);
-    //Serial.println(" microseconds");
-    
-    //counter = 0;  //reset the counter
-    //timer = micros();  //reset the timer base
-  //}
-  //counter++;
-  //encoderCounter++;
-  //delay(500);
-  
+  Serial.println(encoderValueRaw);
   encoderVal= encoderValueRaw;
+}
+
+//checkSum function
+bool checkSum(char cmd[]){
+  //checks whether the input is a valid signal
+  String numcheck = String(cmd[2])+String(cmd[3])+String(cmd[4]);
+  int number = numcheck.toInt();
+  Serial.print("The check is: ");
+  Serial.println(number);
+  if (cmd[0]+cmd[1]==number){
+    Serial.print("Inside the array there is: ");
+    for(int i = 0; i < 2; i++) {
+      Serial.print(cmd[i]);
+    }
+    return true;    //if signal is VALID
+  }
+  else {
+    return false;   //if signal is CORRUPTED
+  }
 }
