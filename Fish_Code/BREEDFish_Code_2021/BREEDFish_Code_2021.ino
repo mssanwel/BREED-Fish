1b843d9266f5b4d32b8e6c60fb7d0e777f773814
Heidi-Chui <Heidi-Chui@github.com>
2021-03-24 17:43:07 +0800

    Combined encoder and fins

--- Fish_Code/BREEDFish_Code_2021/BREEDFish_Code_2021.ino
+++ Fish_Code/BREEDFish_Code_2021/BREEDFish_Code_2021.ino
@@ -1,112 +1,167 @@
-/* 
-    *  Fish Encoder
-    *  
-    *  Receives instructions from transmitter for turning and power values for the fish tail
-    *  
-    *  Modified 1 Mar 2021
-    *  Modified by Allison, Brianna, Heidi
-    *  
-*/
 
-//Arduino Pin Declarations
+/*
+ * Fish encoder + servo fins (receiver side)
+ * 
+ * Description: Receive 4 signals from transmittor 
+ *       R (Left-right fins), U (Up-down fins), T (Turn tail), P (power))
+ * 
+ * Edited by Tasnia, Brianna, Heidi, Allison
+ */
+
+#include <SD.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+//Servo
+Servo servo1;
+Servo servo2;
+int servoPin1 = 5;
+int servoPin2 = 3;
+String letter;
+int num = 100;
+int s1, s2;
+int initial1 = 90;
+int initial2 = 90;
+int min_angle = 0;
+int max_angle = 180;
+int mid_angle = 90;
+int min_range = 0;
+int max_range = 9;
+int mid_range = 4;
+bool check;
+//Encoder
 unsigned int pwm_Pin = 9;
 unsigned int motor_Pwm = 0;
-
+char cmd[11];    //to store the signal from transmitter
 bool red_Flag = LOW;
 bool on = LOW;
 unsigned long int timer1 = 0;
 unsigned long int timer2 = 0;
-
-char cmd[11];    //to store the signal
-
-long int counter =0;
+long int counter = 0;
 long int runtime = 0;
 long int timer = 0;
 float timeStep = 0;
 long int encoderCounter = 0;
-double angularVelocity[3] = {0,0,0};
-int turnVal=5;          //Stores commmand value for control left and right. values 1-4 are left, 5 is straight, 6-9 are right
-int oldTurnVal=5;
-int encoderVal=0;           //stores value of encoder at any given time. Updates from interrupt.
-//int oldEncoderVal;
-int power=5;            //Stores power value from controller
-int oldPower=5;
-long int tailTimer1=millis();
-//long int tailTimer2=millis();   //time when power is reduced (when applicable)
-long int tailDelay1=2000;
-//long int tailDelay2=1000;       //time to occurrence of fish stalling
-int encoderPin0   =  29;
+double angularVelocity[3] = {0, 0, 0};
+int turnVal = 5;        //Stores commmand value for control left and right. values 1-4 are left, 5 is straight, 6-9 are right
+int oldTurnVal = 5;
+int encoderVal = 0;         //stores value of encoder at any given time. Updates from interrupt.
+int power = 9;          //Stores power value from controller
+int oldPower = 5;
+long int tailTimer1 = millis(); //timer for kill switch
+long int tailDelay1 = 2000;
+int encoderPin0   =  29;  
 double diff = 1.0;              //to store the differential value corresponding to the signal
-double stepDiff = 0.1;          //sets the differential value
-
-int highcutoff, lowcutoff, offset, num;
-
-//bool slow=false;
+double stepDiff = 0.2;          //sets the differential value
+int highcutoff, lowcutoff, offset;
 
 void setup() {
- // Define the Serial ports, with Serial1 as the Receiver Port for Data from the transmitter. 
- // The RC is defined as 8 bits, Odd parity, 1 check digit. Default Arduino setting is SERIAL_8N1, where N means no parity.
- 
- // Set the PWM frequency of Timer 2 to be 3kHz
- TCCR2B = TCCR2B & 0b11111000 | 0x01;
-
- //Define the output pins
- pinMode(pwm_Pin, OUTPUT);
- pinMode(13, OUTPUT);
-
-  Serial1.begin(9600,SERIAL_8O1);
-  Serial.begin(9600);
-  //Serial.begin(115200);
-  //Serial.println("Serial Port Initialized!");
-
- 
-  for(int encoderPin = encoderPin0; encoderPin <= 47;encoderPin = encoderPin + 2){   //absolute encoder pin setup
-    pinMode(encoderPin,INPUT_PULLUP);
+  Serial.begin(9600, SERIAL_8O1);
+  Serial1.begin(9600);
+  pinMode(servoPin1, OUTPUT);
+  pinMode(servoPin2, OUTPUT);
+  pinMode(pwm_Pin, OUTPUT);
+  pinMode(13, OUTPUT);
+  servo1.write(initial1);
+  servo2.write(initial2);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  for (int encoderPin = encoderPin0; encoderPin <= 47; encoderPin = encoderPin + 2) { //absolute encoder pin setup
+    pinMode(encoderPin, INPUT_PULLUP);
     Serial.println(encoderPin);
   }
-  
-  while(!Serial.read());
+  while (!Serial.read());
   timer = micros();
-  
-
   Serial.println("Encoder Pins Initialized!");
   Serial.println("Setup is complete! Click to begin the program....");
-
   encoder();      //reads the offset value
   delay(1000);
-  offset=encoderVal;
+  offset = encoderVal;
   Serial.print("Offset= ");
   Serial.println(offset);
+  
+  Serial.println("I'm ready for the code!");
 }
-
+//function for check sum
+bool checkSum(char incomingByte, int siglen, char cmd[]) {
+  Serial.println();
+    String numcheck = String(cmd[2]) + String(cmd[3]) + String(cmd[4]);
+    int number = numcheck.toInt();
+    if (cmd[0] + cmd[1] == number) {
+      Serial.print("Inside the array there is: ");
+      for (int i = 0; i < 2; i++) {
+        Serial.print(cmd[i]);
+      }
+      check = true;
+    } else {
+      check = false;
+    }
+    Serial.println(check);
+    return check;
+}
 void loop() {
-    
   char incomingByte;
   int siglen = 0;  //to store the length of the incoming signal
-  
+  char cmd[11];
   if (Serial1.available() > 0) {  //if more than 2 bytes available to read in the buffer
     Serial.print("I'm receiving: ");
+    // read the incoming byte:
     incomingByte = Serial1.read();
-    while (incomingByte != '?') {  //read char by char until we know the signal is completely read indicated by the identifier '?'
+    while (incomingByte != '?') {  //read char by char until we know the end of signal is reached indicated by the identifier '?'
       cmd[siglen] = incomingByte;
       Serial.print(incomingByte);
       siglen++;
+//    Serial.println("take reading next");
       incomingByte = Serial1.read();
-      Serial1.flush();
     }
-  }
-
-  Serial.println();
-  if (checkSum(cmd)==true){
-    //continues only if signal is valid
     
-    tailTimer1 = millis();
-    interpretSignal();
-    //Kill switch
-    killswitch();
-    encoder();    //reads the current position of the shaft
-    if (oldTurnVal != turnVal || oldPower != power){
-    // sets the range of values for different motor power to generate tail motion corresponding to input signal
+    check = checkSum(incomingByte, siglen, cmd);
+  }
+    if (check == true) {
+      tailTimer1 = millis();
+      //array[0] is 'R' or 'U' or 'T' or 'P' - convert to string, array[1:] are the numbers - convert to integer
+      String w = String(cmd);
+      String letter = String(w[0]);
+      num = String(w.substring(1, 2)).toInt();
+      Serial.println();
+      Serial.print("The number for movement is: ");
+      Serial.println(num);
+      if (letter == "T") {
+        turnVal = num; // tail turning signal
+      }
+      if (letter == "P") {
+        power = num ; // power signal
+      }
+      if (letter == "R") { //for turning the fins left and right (servo motors turn in opposite directions)
+        if (num >= min_range && num < mid_range) {
+          s1 = map(num, min_range, mid_range - 1, min_angle, mid_angle); //left
+          s2 = map(num, min_range, mid_range - 1, max_angle, mid_angle);
+        }
+        else if (num > mid_range + 1 && num <= max_range) { //right
+          s1 = map(num, mid_range + 2, max_range, mid_angle, max_angle);
+          s2 = map(num, mid_range + 2, max_range, mid_angle, min_angle);
+        }
+        else if (num >= 4 and num <= 5) { //no movement
+          s1 = 90;
+          s2 = 90;
+        }
+      }
+      if (letter == "U") { //for moving the fins up and down (servo motors turn in same directions)
+        if (num >= 0 && num < 4) {
+          s1 = map(num, min_range, mid_range - 2, min_angle, mid_angle); //left
+          s2 = map(num, min_range, mid_range - 1, min_angle, mid_angle);
+        }
+        else if (num > 5 && num <= 9) { //right
+          s1 = map(num, mid_range + 2, max_range, mid_angle, max_angle);
+          s2 = map(num, mid_range + 2, max_range, mid_angle, max_angle);
+        }
+        else if (num >= 4 and num <= 5) { //no movement
+          s1 = 90;
+          s2 = 90;
+        }
+      }
+      killswitch();
+      encoder();    //reads the current position of the shaft
       if ((offset + 512) >1023){
        lowcutoff = offset+512-1023;
        highcutoff = offset;
@@ -120,7 +175,7 @@ void loop() {
       Serial.print("lowcutoff:");
       Serial.println(lowcutoff);
       
-      // Turning control Left
+      // tail turning left
       if ((turnVal>=1) && (turnVal<=4)){
         Serial.println("Turning Left");
         diff = 1-(stepDiff*(5-turnVal));
@@ -133,13 +188,13 @@ void loop() {
         }
       }
       
-      // Going Straight
+      // tail going start
       else if (turnVal==5){
         Serial.println("Going straight");
         motor_Pwm = map(power, 0,9,0,255); //maps the value received (0-9) to (0-255)
       }
       
-      // Turning control Right
+      // tail turning right
       else if ((turnVal>=6) && (turnVal<=9)){
         Serial.println("Turning Right");
         diff = 1.0-(stepDiff*(turnVal-5));
@@ -150,87 +205,54 @@ void loop() {
           motor_Pwm = map(power, 0,9,0,255);
         }
       }
+      //print out encoder output and power
       analogWrite (pwm_Pin, motor_Pwm);
       Serial.print("motor pwm: ");
       Serial.println(motor_Pwm);
       oldTurnVal = turnVal;
       oldPower = power;
-//      oldEncoderVal = encoderVal;
+      Serial.print("Motor:");
+      Serial.print(motor_Pwm);
+      //print out coordinates/angles of servo motors
+      Serial.print("\n");
+      Serial.print("output to servo1: ");
+      Serial.print(s1);
+      Serial.print("\n");
+      Serial.print("output to servo2: ");
+      Serial.print(s2);
+      Serial.print("\n");
+      //motors turn according to signal and go back to original position
+      servo1.write(s1);
+      servo2.write(s2);
+    } else {
+      Serial.print("Rejected!");
     }
+    delay(20);
   }
-}
 
 
+//encoder function
 void encoder() {
   // returns the position of the shaft (0-1023)
   // put your main code here, to run repeatedly:
   int a[10];
   int b[10];
-  for(int n = 0 ; n<10 ; n++ ){
-    a[n] = !digitalRead(encoderPin0 + 2*n);
+  for (int n = 0 ; n < 10 ; n++ ) {
+    a[n] = !digitalRead(encoderPin0 + 2 * n);
   }
   b[9] = a[9];
-  for(int i = 1;i<10;i++){
-    b[9-i] = b[9-i+1]^a[9-i];  // xor
+  for (int i = 1; i < 10; i++) {
+    b[9 - i] = b[9 - i + 1] ^ a[9 - i]; // xor
   }
-  float encoderValueRaw = 512.0*b[9]+256.0*b[8]+128.0*b[7]+64.0*b[6]+32.0*b[5]+16.0*b[4]+8.0*b[3]+4.0*b[2]+2.0*b[1]+b[0];
+  float encoderValueRaw = 512.0 * b[9] + 256.0 * b[8] + 128.0 * b[7] + 64.0 * b[6] + 32.0 * b[5] + 16.0 * b[4] + 8.0 * b[3] + 4.0 * b[2] + 2.0 * b[1] + b[0];
   Serial.print("encodervalue : ");
   Serial.println(encoderValueRaw);
-  encoderVal= encoderValueRaw;
+  encoderVal = encoderValueRaw;
 }
 
-//checkSum function
-bool checkSum(char cmd[]){
-  //checks whether the input is a valid signal
-  String numcheck = String(cmd[2])+String(cmd[3])+String(cmd[4]);
-  int number = numcheck.toInt();
-  Serial.print("The check is: ");
-  Serial.println(number);
-  if (cmd[0]+cmd[1]==number){
-    Serial.print("Inside the array there is: ");
-    for(int i = 0; i < 2; i++) {
-      Serial.print(cmd[i]);
-    }
-    return true;    //if signal is VALID
-  }
-  else {
-    return false;   //if signal is CORRUPTED
-  }
-}
-
-//interpret signal
-void interpretSignal(){
-   //array[0] is 'T' or 'U' - convert to string, array[1:] are the numbers - convert to integer  
-   String w = String(cmd);
-   String letter = String(w[0]);
-   num = String(w.substring(1)).toInt();
-   Serial.print("The number for movement is: ");
-   Serial.println(num);
-   if (letter=="T"){
-      turnVal=num; // turning signal
-   }
-   if (letter=="P"){
-      power = num ; // power signal    
-   }
-}
-
-//kill switch if new signal is not received for 3 
-void killswitch(){
+//kill switch if new signal is not received for 3
+void killswitch() {
   if (millis() - tailTimer1 > 3000) {
-      motor_Pwm = 0; 
+    motor_Pwm = 0;
   }
 }
-
-//void fishStalling(){
-//  if (slow==false){
-//    tailTimer2 = millis();
-//  }
-//  if ((millis()-tailTimer2) >= tailDelay2 && (oldEncoderVal==encoderVal)){
-//    motor_Pwm = map(power, 0,9,0,255);
-//  }
-//  else{
-//    motor_Pwm = map(power, 0,9,0,255) * diff;
-//  }
-//  slow = true;
-//  tailTimer2 = millis();
-//}
--- Servo_Fin/Servo_Fin/FINAL_TRANSMITTER.ino
+++ Servo_Fin/Servo_Fin/FINAL_TRANSMITTER.ino
@@ -1,47 +1,47 @@
-int sensorValueX = analogRead(A2);
-int sensorValueY = analogRead(A3);
-int ascii_sumX;
-int ascii_sumY;
-int xMap, yMap;
-String xstr, ystr, powstr;
-int timer1 = 0;
-char signal1[20], signal2[20];
-void setup() {
-  // Begin the Serial at 9600 Baud
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  pinMode(sensorValueX, INPUT);
-  pinMode(sensorValueY, INPUT);
-}
-void loop() {
-  if (millis() - timer1 > 200) {
-    timer1=millis();
-    sensorValueX = analogRead(A2);
-    sensorValueY = analogRead(A3);
-    xMap = map(sensorValueX, 0, 1023, 0, 9);
-    yMap = map(sensorValueY, 0, 1023, 0, 9);
-    ascii_sumX = 84 + (xMap + '0');
-    ascii_sumY = 85 + (yMap + '0');
-    xstr = "R" + String(xMap) + String(ascii_sumX) + "?" ;
-    ystr = "U" + String(yMap) + String(ascii_sumY) + "?";
-    strcpy(signal1, xstr.c_str());
-    strcpy(signal2, ystr.c_str());
-    if ((xMap>5 || xMap<4) && yMap<=5 && yMap>=4) { //if R is greater than 5 or less than 4 and U value is 4-5, only T signal is sent
-          Serial1.write(signal1);
-          Serial.println("Left-right signal:");
-          Serial.println(signal1);
-    }
-    if (xMap<=5 && xMap>=4 && (yMap>5 || yMap<4)) { //if U is greater than 5 or less than 4 and T value is 4-5, only U signal is sent
-          Serial1.write(signal2);
-          Serial.println("Up-down signal:");
-          Serial.println(signal2);
-    }
-    if (xMap<=5 && xMap>=4 && yMap<=5 && yMap>=4) { //If both T and U values are 4-5, only one signal is sent to bring fins back to original position
-          Serial1.write(signal1);
-          Serial.println("Original position signal:");
-          Serial.println(signal1);
-    }
-    //blocking function, no need to wait for delay if use flush
-    Serial1.flush();
-  }
-}
+//int sensorValueX = analogRead(A2);
+//int sensorValueY = analogRead(A3);
+//int ascii_sumX;
+//int ascii_sumY;
+//int xMap, yMap;
+//String xstr, ystr, powstr;
+//int timer1 = 0;
+//char signal1[20], signal2[20];
+//void setup() {
+//  // Begin the Serial at 9600 Baud
+//  Serial.begin(9600);
+//  Serial1.begin(9600);
+//  pinMode(sensorValueX, INPUT);
+//  pinMode(sensorValueY, INPUT);
+//}
+//void loop() {
+//  if (millis() - timer1 > 200) {
+//    timer1=millis();
+//    sensorValueX = analogRead(A2);
+//    sensorValueY = analogRead(A3);
+//    xMap = map(sensorValueX, 0, 1023, 0, 9);
+//    yMap = map(sensorValueY, 0, 1023, 0, 9);
+//    ascii_sumX = 84 + (xMap + '0');
+//    ascii_sumY = 85 + (yMap + '0');
+//    xstr = "R" + String(xMap) + String(ascii_sumX) + "?" ;
+//    ystr = "U" + String(yMap) + String(ascii_sumY) + "?";
+//    strcpy(signal1, xstr.c_str());
+//    strcpy(signal2, ystr.c_str());
+//    if ((xMap>5 || xMap<4) && yMap<=5 && yMap>=4) { //if R is greater than 5 or less than 4 and U value is 4-5, only T signal is sent
+//          Serial1.write(signal1);
+//          Serial.println("Left-right signal:");
+//          Serial.println(signal1);
+//    }
+//    if (xMap<=5 && xMap>=4 && (yMap>5 || yMap<4)) { //if U is greater than 5 or less than 4 and T value is 4-5, only U signal is sent
+//          Serial1.write(signal2);
+//          Serial.println("Up-down signal:");
+//          Serial.println(signal2);
+//    }
+//    if (xMap<=5 && xMap>=4 && yMap<=5 && yMap>=4) { //If both T and U values are 4-5, only one signal is sent to bring fins back to original position
+//          Serial1.write(signal1);
+//          Serial.println("Original position signal:");
+//          Serial.println(signal1);
+//    }
+//    //blocking function, no need to wait for delay if use flush
+//    Serial1.flush();
+//  }
+//}
